389983d4fc9e7a15c083a28abed6cd5fefb0d8f3
Jakub Korytko <jakub@korytko.me>
2024-10-13 10:15:07 +0200

    Make AddUserButton wrap on smaller screen

--- src/components/ListWrapper.vue
+++ src/components/ListWrapper.vue
@@ -6,7 +6,7 @@ import ListComponent from '@/components/ListComponent.vue'
 
 <template>
   <div class="flex flex-col w-full h-full">
-    <div class="bg-pink-600 basis-2/12 justify-between items-center flex flex-row">
+    <div class="bg-pink-600 basis-2/12 justify-between md:items-center flex flex-col md:flex-row">
       <search-bar />
       <add-user-button />
     </div>
